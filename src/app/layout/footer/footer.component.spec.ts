3dae11e0eea8f5090fc642ce08ed1c3b07c75cbc
little-pinecone <marta.szymek.mail@gmail.com>
2021-09-11 12:55:33 +0200

    feat: update footer component * add footer content * update tests * give 100% height for html, body and default-layout

--- src/app/layout/footer/footer.component.spec.ts
+++ src/app/layout/footer/footer.component.spec.ts
@@ -1,25 +1,37 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { FooterComponent } from './footer.component';
+import {FooterComponent} from './footer.component';
+import {DebugElement} from "@angular/core";
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
+  let debugElement: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FooterComponent ]
+      declarations: [FooterComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    debugElement = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display blog name', () => {
+    const bannerElement: HTMLElement = fixture.nativeElement;
+    expect(bannerElement.textContent).toContain('keep_growing');
+  });
+
+  it('should have date', () => {
+    expect(component.today).toBeDefined();
   });
 });
