41c0aa83b16cff4820e8dae8aeca9bdfc88d9543
little-pinecone <marta.szymek.mail@gmail.com>
2021-09-11 10:54:50 +0200

    test: update tests * add mocking library (ng-mocks) * mock dependencies in tests

--- src/app/layout/default-layout/default-layout.component.spec.ts
+++ src/app/layout/default-layout/default-layout.component.spec.ts
@@ -1,6 +1,10 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { DefaultLayoutComponent } from './default-layout.component';
+import {DefaultLayoutComponent} from './default-layout.component';
+import {MockComponent} from "ng-mocks";
+import {TopNavComponent} from "../top-nav/top-nav.component";
+import {PageContentComponent} from "../page-content/page-content.component";
+import {FooterComponent} from "../footer/footer.component";
 
 describe('DefaultLayoutComponent', () => {
   let component: DefaultLayoutComponent;
@@ -8,9 +12,14 @@ describe('DefaultLayoutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DefaultLayoutComponent ]
+      declarations: [
+        DefaultLayoutComponent,
+        MockComponent(TopNavComponent),
+        MockComponent(PageContentComponent),
+        MockComponent(FooterComponent)
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
--- src/app/layout/page-content/page-content.component.spec.ts
+++ src/app/layout/page-content/page-content.component.spec.ts
@@ -1,6 +1,7 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { PageContentComponent } from './page-content.component';
+import {PageContentComponent} from './page-content.component';
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('PageContentComponent', () => {
   let component: PageContentComponent;
@@ -8,9 +9,10 @@ describe('PageContentComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PageContentComponent ]
+      declarations: [PageContentComponent],
+      imports: [RouterTestingModule]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
