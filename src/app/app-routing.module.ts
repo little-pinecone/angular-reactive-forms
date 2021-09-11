99f2227e620d2b64fbe416e0ba7497c1e941f5a7
little-pinecone <marta.szymek.mail@gmail.com>
2021-09-11 10:20:39 +0200

    feat: define default layout * add placeholders for navigation, page content and footer * add placeholder for introduction page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {IntroductionComponent} from "./pages/introduction/introduction.component";
+import {DefaultLayoutComponent} from "./layout/default-layout/default-layout.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", component: DefaultLayoutComponent, children: [
+      {path: "", component: IntroductionComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './layout/top-nav/top-nav.component';
+import { PageContentComponent } from './layout/page-content/page-content.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { DefaultLayoutComponent } from './layout/default-layout/default-layout.component';
+import { IntroductionComponent } from './pages/introduction/introduction.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent,
+    PageContentComponent,
+    FooterComponent,
+    DefaultLayoutComponent,
+    IntroductionComponent
   ],
   imports: [
     BrowserModule,
